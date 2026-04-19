eb678babdca1ae3a5f09e0f6a12979f553faf1a6
Laroslav7 <microsoftjfdjg7@gmail.com>
2026-04-19 21:00:14 +0300

    first commit

---
+++ Win8.1_Start/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Win8._1_Start;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
