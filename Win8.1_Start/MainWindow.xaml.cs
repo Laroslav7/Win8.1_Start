34c7f8e98ed8692adaf83001870af0a405d6a7cf
Laroslav7 <microsoftjfdjg7@gmail.com>
2026-04-19 22:06:58 +0300

    Update 0.2

--- Win8.1_Start/MainWindow.xaml.cs
+++ Win8.1_Start/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Diagnostics;
 
 namespace Win8._1_Start;
 
@@ -19,5 +21,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void TileUri_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is not FrameworkElement fe)
+            return;
+
+        var uri = fe.Tag as string;
+        if (string.IsNullOrWhiteSpace(uri))
+            return;
+
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = uri,
+                UseShellExecute = true
+            });
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(this, ex.Message, "Launch failed", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
